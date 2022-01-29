bb80da6fc56f35daebd875d7f03d58bbb99bd208
john-reiner <johnreiner1990@gmail.com>
2022-01-29 13:46:59 -0500

    new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,22 @@ class UsersController < ApplicationController
     end
 
     def create
-        byebug
+
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_url(@user), notice: "User was successfully created."
+        else
+            flash[:errors] = @user.errors.full_messages
+            render :new, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :screen_name, :password, :password_confirmation )
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :screen_name, presence: true, uniqueness: true, length: { minimum: 6 }
+    validates :email, presence: true, uniqueness: true
 end
