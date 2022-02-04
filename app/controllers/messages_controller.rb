feaa2d70f23dcf46140f7b23a1377ebe9c4cb131
john-reiner <johnreiner1990@gmail.com>
2022-02-04 08:22:54 -0500

    messages work with hotwire

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,19 +3,17 @@ class MessagesController < ApplicationController
     before_action :authenticate_user
 
     def create
-
         @message = Message.create(
             user_id: @authenticated_user.id,
             chat_id: params[:chat_id],
             content: params[:message][:content]
         )
+
         respond_to do |format|
             if !@message.save
-
                 # format.turbo_stream { render turbo_stream: turbo_stream.replace("message_form", partial: "messages/form", locals: {message: @message})}
                 format.html { render partial: "form" }
             end
         end
     end
-
 end
