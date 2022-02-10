212ceb544f3182fdcd4b117bf44286d66d7fb3a7
john-reiner <johnreiner1990@gmail.com>
2022-02-10 08:46:51 -0500

    routed correctly to the new chat created

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -12,38 +12,28 @@ class ChatsController < ApplicationController
 
     def new
         @chat = Chat.new
-        @message = Message.new
+    end
+
+    def create_user_chat 
+
     end
 
     def create
-
         other_user = User.find_by(screen_name: params[:chat][:screen_name])
-        
         if other_user
-            
             @chat = Chat.create(name: "Chat with #{@authenticated_user.screen_name} and #{other_user.screen_name}")
-            
-            # respond_to do |format|
             if @chat.save
                 @chat.users << @authenticated_user
                 @chat.users << other_user
                 @chat.messages.create(
                     user_id: @authenticated_user.id,
                     chat_id: @chat.id,
-                    content: "#{@authenticated_user.screen_name} joined the chat"
+                    content: "#{@authenticated_user.screen_name} added #{other_user.screen_name} to the chat"
                 )
-                # byebug
-                @chat.messages.create(
-                    user_id: other_user.id,
-                    chat_id: @chat.id,
-                    content: "#{other_user.screen_name} joined the chat"
-                )
-                # format.turbo_stream { render turbo_stream: turbo_stream.append("chat_links", partial: 'shared/chat_link', locals: { chat: @chat }) }
-                else
-                    redirect_to @authenticated_user, notice: "Something went wrong..."
-                end
-            # end
-            
+                redirect_to user_chat_url(@authenticated_user, @chat)
+            else
+                redirect_to @authenticated_user, notice: "Something went wrong..."
+            end
         else
             redirect_to @authenticated_user, notice: "Screen Name '#{params[:chat][:screen_name]}' not found"
         end
