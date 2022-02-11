e75106f0879fdb68f3a283084e3f1e5344ebdba6
john-reiner <johnreiner1990@gmail.com>
2022-02-11 08:23:47 -0500

    everything seems to work: new chats are broadcasted to user show page and new messages are updated on user-show page

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,7 @@ class Message < ApplicationRecord
   after_create_commit -> {
     # line 6 is in the chat show page, so we update the chat show page only once a message is created 
     broadcast_append_to "messages", target: self.chat
-    # broadcast_replace_to "chat_links", target: self.chat, partial: "shared/chat_link", locals: { chat: self.chat, user: self.user }
+    broadcast_replace_to "chat_links", target: self.chat, partial: "shared/chat_link", locals: { chat: self.chat, user: self.user }
   }
 
   validates :content, presence: true
